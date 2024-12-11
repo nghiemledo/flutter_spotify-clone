1c0aad973e738a7d1337653ae0bd0cba4758e475
Pham Ba Duong <pham2008duong2004@gmail.com>
2024-12-11 21:19:42 +0700

    Feat: Add search screen and seearch result screen

--- client/lib/screens/search/search.screen.dart
+++ client/lib/screens/search/search.screen.dart
@@ -2,14 +2,224 @@ import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          "Tìm Kiếm",
-          style: TextStyle(color: Colors.white, fontSize: 24),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        title: const Text(
+          'Tìm kiếm',
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.camera_alt_outlined, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Search Bar
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Row(
+                children: [
+                  Icon(Icons.search, color: Colors.black54),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                        hintText: 'Bạn muốn nghe gì?',
+                        hintStyle: TextStyle(
+                            color: Colors.black54, fontWeight: FontWeight.bold),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+
+            const Text(
+              'Khám phá các thể loại của bạn',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            "#Hip hop",
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'Duyệt tìm tất cả',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                  ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: 2 / 1,
+                children: const [
+                  // All Category Cards
+                  CategoryCard(color: Colors.blue, label: "Nhạc", imageUrl: ""),
+                  CategoryCard(color: Colors.red, label: "Podcast", imageUrl: ""),
+                  CategoryCard(color: Colors.green, label: "Tâm trạng", imageUrl: ""),
+                  CategoryCard(color: Colors.orange, label: "Ngủ", imageUrl: ""),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+//GenreCard
+class GenreCard extends StatelessWidget {
+  final String label;
+  final Color color;
+  final String imageUrl;
+
+  const GenreCard({super.key, required this.label, required this.color, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          // image: DecorationImage(
+          //     image: NetworkImage(imageUrl)
+          // ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "#Hip hop",
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+//CategoryCard
+class CategoryCard extends StatelessWidget {
+  final Color color;
+  final String label;
+  final String imageUrl;
+
+  const CategoryCard(
+      {super.key,
+      required this.color,
+      required this.label,
+      required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  label,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..rotateZ(0.5)
+                    ..translate(25.0, 10.0, 0.0),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      image: DecorationImage(
+                        image: NetworkImage(imageUrl),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- client/lib/screens/search/searchresult.screen.dart
+++ client/lib/screens/search/searchresult.screen.dart
@@ -1,10 +1,61 @@
+import 'package:client/screens/playlist/playlist.screen.dart';
 import 'package:flutter/material.dart';
 
 class SearchResultScreen extends StatelessWidget {
-  const SearchResultScreen({super.key});
+  final TextEditingController _controller = TextEditingController();
+
+  SearchResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.white24,
+          elevation: 0,
+          title: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const SizedBox(width: 10,),
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.close, color: Colors.white),
+                onPressed: () {
+                  _controller.clear();
+                },
+              ),
+            ],
+          ),
+        ),
+
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              return PlayListScreen(
+                title: "Lời Tạm Biệt Chưa Nói",
+                author: "GREY D, Orange, Kai Đinh",
+                imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgFLUCy0cckz2AUEnf6hFfqK3U1vqSjMuK-Q&s",
+                icon: Icons.more_vert,
+              );
+            },
+          ),
+        )
+    );
   }
 }
